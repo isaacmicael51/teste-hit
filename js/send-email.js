d8d1dc00ee3ef3aba0e4debbf1dd51617710d07c
Isaac Spagnol <isaacmicael51@gmail.com>
2022-03-15 10:38:55 -0300

    funcao envia email

--- js/send-email.js
+++ js/send-email.js
@@ -1,21 +1,21 @@
 
 $("#send").click(function (e) {
-//     e.preventDefault();
-//   var name = $("#name").val();
-//   var email = $("#email").val();
-//   var message = $("#message").val();
+    e.preventDefault();
+  var name    = $("#name").val();
+  var email   = $("#email").val();
+  var message = $("#message").val();
 
-//   console.log(name, email, message);
+  console.log(name, email, message);
 
-//   if (name == "" || (null && email == "") || (null && message == "") || null) {
-//     alert("prencha os campos");
-//     return false;
-//   }else {
-//     $.get("https://isaacspagnol.com.br/teste-hit//email.php", {
-//       name: name,
-//       email: email,
-//       message: message,
-//     });
-//   }
-console.log('cloquei');
+  if (name == "" || (null && email == "") || (null && message == "") || null) {
+    alert("prencha os campos");
+    return false;
+  }else {
+    $.get("https://isaacspagnol.com.br/teste-hit/email.php", {
+      name: name,
+      email: email,
+      message: message,
+    });
+  }
+
 });
